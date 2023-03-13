11f778a87226f90b1603e38534280718c5af2aed
Andrzej Opon <andrzejopon3@gmail.com>
2023-03-14 00:15:23 +0100

    project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // write your code here
+        Segment seg = new Segment(new Point(4,5 ),new Point(5,19 ));
+        Segment arr[]=Segment.perpendicular(seg,new Point(5,15));
+        System.out.println(arr[1].getSVG());
     }
 }
---
+++ src/Polygon.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+
+public class Polygon {
+    private Point[] vertices;
+    Style style;
+    Polygon(int numOfVer, Style style){
+        vertices = new Point[numOfVer];
+        this.style = style;
+    }
+    public void setVertex(int numOfVer, Point vertex){
+        vertices[numOfVer] = vertex;
+    }
+    public void setVertices(Point[] vertices){
+        this.vertices = vertices;
+    }
+    public String toSvg(){
+        String s = "";
+        for(Point p: vertices){
+            s = s+p.x+","+p.y+" ";
+        };
+        return String.format(Locale.ENGLISH,"<polygon points=\"%s\" style=\"%s\"/> ",s,style.toSvg());
+    }
+}
